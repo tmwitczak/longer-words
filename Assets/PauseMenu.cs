2cbc3458fb763ec75706e00b49c0629dbc148aad
jan-koz <56533850+jan-koz@users.noreply.github.com>
2019-12-06 17:57:42 +0100

    Checkpoint version

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -2,25 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
+    public string command;
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) )
         {
             if (GameIsPaused)
             {
-                Resume();
+                    Resume();
+                
             }
             else
             {
-                Pause();
+                    Pause();
+              
             }
         }
+        foreach (char c in Input.inputString)
+        {
+            if (c == '\b' && command.Length != 0)
+            {
+                command = command.Substring(0, command.Length - 1);
+            }
+            else if ((c == '\n') || (c == '\r'))
+            {
+                if (command == "resume")
+                {
+                    Resume();
+                }
+                else if (command == "menu")
+                {
+                    loadMenu();
+                }
+                else if (command == "quit")
+                {
+                    QuitGame();
+                }
+                command = "";
+            }
+            else
+            {
+                command += c;
+              
+            }
+        }
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        //    if (GameIsPaused)
+        //    {
+        //        Resume();
+
+        //    }
+        //}
+        //if (Input.GetKeyDown(KeyCode.M))
+        //{
+        //    if (GameIsPaused)
+        //    {
+        //        loadMenu();
+
+        //    }
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    if (GameIsPaused)
+        //    {
+        //        QuitGame();
+
+        //    }
+        //}
     }
 
     public void Resume()
@@ -45,6 +103,7 @@ public class PauseMenu : MonoBehaviour
 
     public void QuitGame()
     {
+        Debug.Log("Quit!");
         Application.Quit();
 
     }
